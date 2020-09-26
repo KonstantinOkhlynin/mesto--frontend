213e8e09077f21b5d365ad5db7a2793b1222af0d
Константин <Kostya.Okhlynin@gmail.com>
2020-09-26 15:43:27 +0300

    Настроил Babel

--- src/index.js
+++ src/index.js
@@ -89,6 +89,12 @@
     })
 })()
 
+
+const numbers = [2, 3, 5];
+const doubledNumbers = numbers.map(number => number * 2); // Стрелочная функция. Не запнётся ли на ней Internet Explorer
+
+console.log(doubledNumbers); // 4, 6, 10 
+
 /*
     Класс Api создан и запросы на сервер выполняются, отлично, что Вы попробовали
     использовать async/await, но с ним есть проблемы:
